fc3dd16d44b5f25d3c40ebcc7545fb32bc1fbea0
Raaum <greg.raaum@yhaoo.com>
2016-01-19 22:04:54 -0600

    Added another fibonacci example

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -8,16 +8,44 @@ the array, using the current value of i as our index position.
 We generate the sequence by computing the sum of the previous 2 items in our
 f array.  Our f array always has a length that is 1 greater than our counter, 
 so we can compute on previous array item.
+
+/* Fibonacci Sequence
+
+  1  +  1  =  2
+  1  +  2  =  3
+  2  +  3  =  5
+  3  +  5  =  8
+  5  +  8  =  13
+  8  +  13 =  21;
+
+1,1,2,3,5,8,13,21,etc.
+
 */
 
-function fibonacci() {
+
+// The formulaic way to write it using an initial seed of 1 value
+
+function fibonacci_example() {
 
   for (f=[1], i=1, f.push(i); i<50; i++) {
     f.push(f[i] + f[i-1]);
   } 
+  document.write(f.toString());  // f.toString() contains the sequence
+}
 
-  // f.toString() contains the sequence
 
-  document.write('<pre>' + f.toString() + '</pre>');
 
+// Another way to write it in which the initial seed requires 2 values
+
+function fibonacci_example2() {
+
+  var fibonacci  = [0,1]; 
+  var max_values = 50;
+
+  for (i = fibonacci.length; i < max_values; i++) {
+
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2]; // push to fibonacci
+
+    document.write(fibonacci[i] + '<br>');
+  }
 }
